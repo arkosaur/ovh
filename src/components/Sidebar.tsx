66eab18458e5ea93ef1cbb99310aa7a4cce4d8f2
coolci <40325889+coolci@users.noreply.github.com>
2025-10-22 18:48:17 +0800

    添加图标

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -131,6 +131,12 @@ const Sidebar = ({ onToggle, isOpen }: SidebarProps) => {
                     <polyline points="10 9 9 9 8 9"></polyline>
                   </>
                 )}
+                {item.icon === "bell" && (
+                  <>
+                    <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                    <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+                  </>
+                )}
                 {item.icon === "settings" && (
                   <>
                     <circle cx="12" cy="12" r="3"></circle>
